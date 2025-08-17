49c6cb6056a2ac17501b51c219270b515b3822be
furaizi <furachannel16@gmail.com>
2025-08-17 14:12:18 +0300

    Bind command implementations to a telegram bot

--- src/main/kotlin/com/download/downloaderbot/bot/config/BotConfig.kt
+++ src/main/kotlin/com/download/downloaderbot/bot/config/BotConfig.kt
@@ -1,9 +1,14 @@
 package com.download.downloaderbot.bot.config
 
+import com.download.downloaderbot.bot.commands.CommandContext
+import com.download.downloaderbot.bot.commands.CommandHandler
+import com.download.downloaderbot.bot.commands.TelegramGateway
 import com.download.downloaderbot.bot.config.properties.BotProperties
 import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
 import mu.KotlinLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -11,13 +16,28 @@ import org.springframework.context.annotation.Configuration
 private val log = KotlinLogging.logger {}
 
 @Configuration
-class BotConfig(val botProperties: BotProperties) {
+class BotConfig(
+    val botProperties: BotProperties,
+    val commands: List<CommandHandler>,
+    val gateway: TelegramGateway
+) {
+
+    private val defaultCommand = commands.first { it.name == "download" }
 
     @Bean
     fun telegramBot(): Bot = bot {
         token = botProperties.token
         dispatch {
+            commands.forEach { handler ->
+                command(handler.name) {
+                    log.info { "Executing command /${handler.name}" }
+                    handler.handle(CommandContext(update, args, gateway))
+                }
+            }
 
+            text {
+                defaultCommand.handle(CommandContext(update, listOf(text), gateway))
+            }
         }
     }
 }
