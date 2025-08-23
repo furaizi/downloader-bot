6ddb38b14e69ce06ec965d06622964f010137c8a
furaizi <furachannel16@gmail.com>
2025-08-24 01:20:23 +0300

    Enable photo album downloading in the bot

--- src/main/kotlin/com/download/downloaderbot/bot/commands/DownloadCommand.kt
+++ src/main/kotlin/com/download/downloaderbot/bot/commands/DownloadCommand.kt
@@ -23,15 +23,27 @@ class DownloadCommand(
         val mediaList = withContext(Dispatchers.IO) {
             mediaDownloadService.download(url)
         }
-        val file = File(media.fileUrl)
+        val firstMedia = mediaList.first()
 
-        when (media.type) {
-            MediaType.VIDEO -> gateway.sendVideo(ctx.chatId, file)
-            MediaType.AUDIO -> gateway.sendAudio(ctx.chatId, file)
-            MediaType.IMAGE -> gateway.sendPhoto(ctx.chatId, file)
+        if (firstMedia.type == MediaType.IMAGE && mediaList.size in 2..10) {
+            val files = mediaList.map { File(it.fileUrl) }
+            gateway.sendPhotosAlbum(ctx.chatId, files)
+        }
+        else if (firstMedia.type == MediaType.IMAGE && mediaList.size > 10) {
+            val files = mediaList.map { File(it.fileUrl) }
+            gateway.sendPhotosAlbumChunked(ctx.chatId, files)
+        }
+        else {
+            mediaList.forEach {
+                val file = File(it.fileUrl)
+                when (it.type) {
+                    MediaType.VIDEO -> gateway.sendVideo(ctx.chatId, file)
+                    MediaType.AUDIO -> gateway.sendAudio(ctx.chatId, file)
+                    MediaType.IMAGE -> gateway.sendPhoto(ctx.chatId, file)
+                }
+            }
         }
 
-        log.info { "Downloaded: ${media.title} (${file.name})" }
-
+        log.info { "Downloaded: ${mediaList.map { it.title }} (${mediaList.map { it.fileUrl }})" }
     }
 }
