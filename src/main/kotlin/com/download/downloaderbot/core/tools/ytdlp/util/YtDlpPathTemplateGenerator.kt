dfdf4aac1b164a542791698150a2037ac472506d
furaizi <furachannel16@gmail.com>
2025-08-23 21:49:53 +0300

    Extract base prefix generation to an util object

--- src/main/kotlin/com/download/downloaderbot/core/tools/ytdlp/util/YtDlpPathTemplateGenerator.kt
+++ src/main/kotlin/com/download/downloaderbot/core/tools/ytdlp/util/YtDlpPathTemplateGenerator.kt
@@ -1,6 +1,7 @@
 package com.download.downloaderbot.core.tools.ytdlp.util
 
 import com.download.downloaderbot.core.config.properties.YtDlpProperties
+import com.download.downloaderbot.core.tools.util.baseprefix.BasePrefixGenerator
 import com.download.downloaderbot.core.tools.util.pathgenerator.DownloadPath
 import com.download.downloaderbot.core.tools.util.pathgenerator.PathTemplateGenerator
 import org.springframework.stereotype.Component
@@ -17,27 +18,9 @@ class YtDlpPathTemplateGenerator(
     private val baseDir = Paths.get(props.baseDir)
 
     override fun generate(url: String): DownloadPath {
-        val basePrefix = generateBasePrefix(url)
+        val basePrefix = BasePrefixGenerator.generate(url)
         val outputTemplate = baseDir.resolve("$basePrefix.%(ext)s")
             .toString()
         return DownloadPath(basePrefix, outputTemplate)
-    }
-
-    private fun generateBasePrefix(url: String): String {
-        val host = getHostName(url) ?: "media"
-        val timestamp = Instant.now().toEpochMilli()
-        val shortUuid = UUID.randomUUID()
-            .toString()
-            .take(8)
-        return "$host-$timestamp-$shortUuid"
-    }
-
-    private fun getHostName(url: String): String? {
-        return runCatching {
-            URI(url).host
-                ?.replace(oldValue = ":", newValue = "-")
-        }
-            .getOrNull()
-            ?.takeIf { it.isNotBlank() }
     }
 }
