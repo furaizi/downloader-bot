11134829a23c377071556944eef282dcf61abf06
furaizi <furachannel16@gmail.com>
2025-08-24 13:07:01 +0300

    Add a universal caching interface

---
+++ src/main/kotlin/com/download/downloaderbot/core/cache/MediaCache.kt
@@ -0,0 +1,10 @@
+package com.download.downloaderbot.core.cache
+
+import com.download.downloaderbot.core.domain.Media
+import java.time.Duration
+
+interface MediaCache {
+    suspend fun getBySourceUrl(url: String): Media?
+    suspend fun put(media: Media, ttl: Duration = Duration.ofDays(7)) // TODO: make ttl configurable
+    suspend fun evict(sourceUrl: String)
+}
