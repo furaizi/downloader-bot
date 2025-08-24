f171861b213172aa534473e0a2af81a00e49b4ac
furaizi <furachannel16@gmail.com>
2025-08-24 12:54:55 +0300

    Configure the JSON serializator for correct OffsetDateTime handling

--- src/main/kotlin/com/download/downloaderbot/core/config/MappingConfig.kt
+++ src/main/kotlin/com/download/downloaderbot/core/config/MappingConfig.kt
@@ -1,6 +1,8 @@
 package com.download.downloaderbot.core.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,5 +11,8 @@ import org.springframework.context.annotation.Configuration
 class MappingConfig {
 
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper().registerKotlinModule()
+    fun objectMapper(): ObjectMapper = ObjectMapper()
+        .registerKotlinModule()
+        .registerModule(JavaTimeModule())
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 }
