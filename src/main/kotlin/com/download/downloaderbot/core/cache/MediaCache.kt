e9128594b219c97b5a655d6ce81de99b9759c6f3
furaizi <furachannel16@gmail.com>
2025-08-24 13:11:46 +0300

    Add a RedisMediaCache implementation

--- src/main/kotlin/com/download/downloaderbot/core/cache/MediaCache.kt
+++ src/main/kotlin/com/download/downloaderbot/core/cache/MediaCache.kt
@@ -4,7 +4,7 @@ import com.download.downloaderbot.core.domain.Media
 import java.time.Duration
 
 interface MediaCache {
-    suspend fun getBySourceUrl(url: String): Media?
+    suspend fun getBySourceUrl(sourceUrl: String): Media?
     suspend fun put(media: Media, ttl: Duration = Duration.ofDays(7)) // TODO: make ttl configurable
     suspend fun evict(sourceUrl: String)
 }
