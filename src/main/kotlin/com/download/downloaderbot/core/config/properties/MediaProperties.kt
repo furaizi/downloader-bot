9e323461baa8ce1cce3dce32d573e5ffb15c7016
furaizi <furachannel16@gmail.com>
2025-08-30 15:49:09 +0300

    feat(config): add media size limit properties for photo and video

--- src/main/kotlin/com/download/downloaderbot/core/config/properties/MediaProperties.kt
+++ src/main/kotlin/com/download/downloaderbot/core/config/properties/MediaProperties.kt
@@ -1,11 +1,18 @@
 package com.download.downloaderbot.core.config.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.util.unit.DataSize
 import java.nio.file.Paths
 
 @ConfigurationProperties(prefix = "downloader.media")
 data class MediaProperties(
-    val baseDir: String
+    val baseDir: String,
+    val maxSize: MediaSizeLimits
 ) {
     val basePath = Paths.get(baseDir)
 }
+
+data class MediaSizeLimits(
+    val photo: DataSize = DataSize.ofMegabytes(10),
+    val video: DataSize = DataSize.ofMegabytes(50)
+)
