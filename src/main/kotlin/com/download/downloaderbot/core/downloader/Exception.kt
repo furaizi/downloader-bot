5811f31d595bbc58a0e39def92b16d427a1eff2d
furaizi <furachannel16@gmail.com>
2025-08-30 15:47:19 +0300

    feat(core): add MediaTooLargeException for oversized media files

---
+++ src/main/kotlin/com/download/downloaderbot/core/downloader/Exception.kt
@@ -0,0 +1,15 @@
+package com.download.downloaderbot.core.downloader
+
+import com.download.downloaderbot.core.domain.MediaType
+
+private const val bytesInMB = 1024 * 1024
+private fun Long.toMB() = this / bytesInMB
+
+class MediaTooLargeException(
+    val actualSize: Long,
+    val limit: Long,
+    val mediaType: MediaType
+) : RuntimeException(
+    "Media file of type $mediaType exceeds the size limit. " +
+            "Actual size: ${actualSize.toMB()} MB, Limit: ${limit.toMB()} bytes"
+)
