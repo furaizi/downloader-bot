a7b36734b4c177416594eea46e51ca314c15f97c
furaizi <furachannel16@gmail.com>
2025-09-15 21:08:46 +0300

    refactor(mapper): use new API for building ObjectMapper

--- src/main/kotlin/com/download/downloaderbot/core/config/MappingConfig.kt
+++ src/main/kotlin/com/download/downloaderbot/core/config/MappingConfig.kt
@@ -1,9 +1,11 @@
 package com.download.downloaderbot.core.config
 
+import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,8 +13,10 @@ import org.springframework.context.annotation.Configuration
 class MappingConfig {
 
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper()
-        .registerKotlinModule()
-        .registerModule(JavaTimeModule())
-        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    fun objectMapper(): ObjectMapper = JsonMapper.builder()
+            .addModule(KotlinModule.Builder().build())
+            .addModule(JavaTimeModule())
+            .enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .build()
 }
