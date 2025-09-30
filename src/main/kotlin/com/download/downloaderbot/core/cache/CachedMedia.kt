eb1187c3d557cae9d2b30de9c57692046e366b14
furaizi <furachannel16@gmail.com>
2025-09-30 23:53:20 +0300

    refactor(cache): store full Media object in CachedMedia instead of MediaType

--- src/main/kotlin/com/download/downloaderbot/core/cache/CachedMedia.kt
+++ src/main/kotlin/com/download/downloaderbot/core/cache/CachedMedia.kt
@@ -1,9 +1,9 @@
 package com.download.downloaderbot.core.cache
 
-import com.download.downloaderbot.core.domain.MediaType
+import com.download.downloaderbot.core.domain.Media
 
 data class CachedMedia(
-    val type: MediaType,
+    val media: Media,
     val fileUniqueId: String,
     val lastFileId: String
 )
