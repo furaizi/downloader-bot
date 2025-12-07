849bf19450f061f62c02841407fe9929dd237871
furaizi <furachannel16@gmail.com>
2025-12-07 16:15:40 +0200

    test: add Testcontainers (BOM + JUnit Jupiter integration)

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
     testImplementation("io.kotest:kotest-property-jvm:5.9.1")
+    testImplementation(platform("org.testcontainers:testcontainers-bom:1.21.3"))
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
