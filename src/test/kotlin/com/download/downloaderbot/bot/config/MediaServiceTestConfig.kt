5d1060490470011ab7cb75238aeb8ba69450d755
furaizi <furachannel16@gmail.com>
2025-12-07 22:38:15 +0200

    test: add integration tests and test config for MediaServiceImpl

---
+++ src/test/kotlin/com/download/downloaderbot/bot/config/MediaServiceTestConfig.kt
@@ -0,0 +1,21 @@
+package com.download.downloaderbot.bot.config
+
+import com.download.downloaderbot.core.downloader.MediaProvider
+import com.download.downloaderbot.infra.network.StubFinalUrlResolver
+import io.mockk.mockk
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+
+@TestConfiguration
+class MediaServiceTestConfig {
+
+    @Bean
+    @Primary
+    fun mediaProviderMock(): MediaProvider = mockk(relaxed = true)
+
+    @Primary
+    @Bean
+    fun stubFinalUrlResolver() = StubFinalUrlResolver()
+
+}
