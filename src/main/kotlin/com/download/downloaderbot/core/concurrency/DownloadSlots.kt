86fe7483c3a7f2fcb297174a9f71588c5955a37b
furaizi <furachannel16@gmail.com>
2025-12-16 23:25:02 +0200

    refactor: remove unused DownloadSlot interface

--- src/main/kotlin/com/download/downloaderbot/core/concurrency/DownloadSlots.kt
+++
@@ -1,8 +0,0 @@
-package com.download.downloaderbot.core.concurrency
-
-interface DownloadSlots {
-    suspend fun <T> withSlotOrThrow(
-        url: String,
-        block: suspend () -> T,
-    ): T
-}
