91a5a814d8806c846ad6e1392818502ceff29724
furaizi <furachannel16@gmail.com>
2025-12-16 22:24:08 +0200

    test: refactor end-to-end tests

--- src/test/kotlin/com/download/downloaderbot/e2e/E2ETestConfig.kt
+++ src/test/kotlin/com/download/downloaderbot/e2e/E2ETestConfig.kt
@@ -1,17 +1,20 @@
 package com.download.downloaderbot.e2e
 
 import com.download.downloaderbot.bot.gateway.RecordingBotPort
+import com.download.downloaderbot.core.net.FinalUrlResolver
 import com.download.downloaderbot.infra.di.ForYtDlp
 import com.download.downloaderbot.infra.network.StubFinalUrlResolver
 import com.download.downloaderbot.infra.process.runner.FailingProcessRunner
+import com.download.downloaderbot.infra.process.runner.FakeProcessRunner
 import com.download.downloaderbot.infra.process.runner.ProcessRunner
-import com.download.downloaderbot.core.net.FinalUrlResolver
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
 
 @TestConfiguration
-class DownloadFailureTestConfig {
+class E2ETestConfig {
+
     @Bean
     @Primary
     fun recordingBotPort() = RecordingBotPort()
@@ -23,5 +26,12 @@ class DownloadFailureTestConfig {
     @Bean
     @Primary
     @ForYtDlp
+    @ConditionalOnProperty(name = ["downloader.yt-dlp.runner"], havingValue = "fake", matchIfMissing = true)
+    fun fakeYtDlpRunner(): ProcessRunner = FakeProcessRunner()
+
+    @Bean
+    @Primary
+    @ForYtDlp
+    @ConditionalOnProperty(name = ["downloader.yt-dlp.runner"], havingValue = "fail")
     fun failingYtDlpRunner(): ProcessRunner = FailingProcessRunner()
 }
