1f077f8a891c09c66b4b7f745342d9d3094b6cf6
furaizi <furachannel16@gmail.com>
2025-12-18 19:21:00 +0200

    fix: do not require telegram bot token when launching tests

--- src/main/kotlin/com/download/downloaderbot/bot/config/StartupChecks.kt
+++ src/main/kotlin/com/download/downloaderbot/bot/config/StartupChecks.kt
@@ -4,8 +4,10 @@ import com.download.downloaderbot.bot.config.properties.BotProperties
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("!test")
 class StartupChecks {
     @Bean
     fun checkToken(props: BotProperties) =
